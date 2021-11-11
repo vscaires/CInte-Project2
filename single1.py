0980c275def179200bd145b41eaec2db8556df40
vscaires <vscaires@hotmail.com>
2021-11-11 17:54:01 +0000

    single objective works

---
+++ single1.py
@@ -0,0 +1,132 @@
+from deap import base
+from deap import creator
+from deap import tools
+import random
+import pandas as pd
+
+data = pd.read_csv("CityDistCar.csv")
+data = data.drop(columns=["Distances of Cities by Car (min)"])
+
+
+num_cities=20
+
+creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+creator.create("Individual", list, fitness=creator.FitnessMin)
+
+toolbox = base.Toolbox()
+toolbox.register("rand_city", random.sample, range(num_cities), num_cities)
+toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.rand_city)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+def city_distance(individual):
+    data = pd.read_csv("CityDistCar.csv")
+    data = data.drop(columns=["Distances of Cities by Car (min)"])
+    
+    distance = 0
+    distance = data.iloc[individual[0], individual[-1]]
+    
+    for i in range(num_cities - 1):
+        distance += data.iloc[individual[i], individual[i+1]]
+    
+    return distance,
+
+
+toolbox.register("evaluate", city_distance)
+toolbox.register("mate", tools.cxOrdered)
+toolbox.register("mutate", tools.mutShuffleIndexes, indpb=0.05)
+toolbox.register("select", tools.selTournament, tournsize=5)
+
+
+
+def main():
+    random.seed(64)
+
+    # create an initial population of 300 individuals (where
+    # each individual is a list of integers)
+    pop = toolbox.population(n=250)
+    
+    # CXPB  is the probability with which two individuals
+    #       are crossed
+    #
+    # MUTPB is the probability for mutating an individual
+    CXPB, MUTPB = 0.5, 0.2
+    
+    print("Start of evolution")
+    
+    # Evaluate the entire population
+    fitnesses = list(map(toolbox.evaluate, pop))
+    for ind, fit in zip(pop, fitnesses):
+        ind.fitness.values = fit
+    
+    print("  Evaluated %i individuals" % len(pop))
+
+    # Extracting all the fitnesses of 
+    fits = [ind.fitness.values[0] for ind in pop]
+
+    # Variable keeping track of the number of generations
+    g = 0
+    
+    # Begin the evolution
+    while g < 40:
+        # A new generation
+        g = g + 1
+        print("-- Generation %i --" % g)
+        
+        # Select the next generation individuals
+        offspring = toolbox.select(pop, len(pop))
+        # Clone the selected individuals
+        offspring = list(map(toolbox.clone, offspring))
+    
+        # Apply crossover and mutation on the offspring
+        for child1, child2 in zip(offspring[::2], offspring[1::2]):
+
+            # cross two individuals with probability CXPB
+            if random.random() < CXPB:
+                toolbox.mate(child1, child2)
+
+                # fitness values of the children
+                # must be recalculated later
+                del child1.fitness.values
+                del child2.fitness.values
+
+        for mutant in offspring:
+
+            # mutate an individual with probability MUTPB
+            if random.random() < MUTPB:
+                toolbox.mutate(mutant)
+                del mutant.fitness.values
+    
+        # Evaluate the individuals with an invalid fitness
+        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+        fitnesses = map(toolbox.evaluate, invalid_ind)
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit
+        
+        print("  Evaluated %i individuals" % len(invalid_ind))
+        
+        # The population is entirely replaced by the offspring
+        pop[:] = offspring
+        
+        # Gather all the fitnesses in one list and print the stats
+        fits = [ind.fitness.values[0] for ind in pop]
+        
+        length = len(pop)
+        mean = sum(fits) / length
+        sum2 = sum(x*x for x in fits)
+        std = abs(sum2 / length - mean**2)**0.5
+        
+        print("  Min %s" % min(fits))
+        print("  Max %s" % max(fits))
+        print("  Avg %s" % mean)
+        print("  Std %s" % std)
+        
+        best_ind = tools.selBest(pop, 1)[0]
+        print("Gen %d: best individual is %s, %s" % (g, best_ind, best_ind.fitness.values))
+    
+    print("-- End of (successful) evolution --")
+    
+    best_ind = tools.selBest(pop, 1)[0]
+    print("Best individual is %s, %s" % (best_ind, best_ind.fitness.values))
+
+if __name__ == "__main__":
+    main()
