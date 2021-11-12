bc22ba5655f354782f60879a5b3c6387fa55ba55
vscaires <vscaires@hotmail.com>
2021-11-12 16:47:26 +0000

    added runs with multiple random seeds

--- costcar.py
+++ costcar.py
@@ -14,6 +14,8 @@ positions = positions.drop(columns=["City"])
 NUM_CITIES = 20
 POPULATION = 40
 GENERATIONS = 250
+MAX_RUNS = 30
+
 
 positions.drop(positions.tail(50 - NUM_CITIES).index,inplace=True) # drop last n rows
 
@@ -44,100 +46,113 @@ toolbox.register("select", tools.selTournament, tournsize=3)
 
 
 def main():
-    random.seed(48)
 
-    # create an initial population of 300 individuals (where
-    # each individual is a list of integers)
-    pop = toolbox.population(n=POPULATION)
-    
-    # CXPB  is the probability with which two individuals
-    #       are crossed
-    #
-    # MUTPB is the probability for mutating an individual
-    CXPB, MUTPB = 0.9, 0.3
-    
-    print("Start of evolution")
-    
-    # Evaluate the entire population
-    fitnesses = list(map(toolbox.evaluate, pop))
-    for ind, fit in zip(pop, fitnesses):
-        ind.fitness.values = fit
-    
-    print("  Evaluated %i individuals" % len(pop))
+    overall_mean = 0
+    overall_std = 0
+    for rnd in range(1, MAX_RUNS):
+        random.seed(rnd)
 
-    # Extracting all the fitnesses of 
-    fits = [ind.fitness.values[0] for ind in pop]
+        print("-- RUN %i --" % rnd)
 
-    # Variable keeping track of the number of generations
-    g = 0
-    
-    bestGen = []
-    
-    # Begin the evolution
-    while g < GENERATIONS:
-
-        # A new generation
-        g = g + 1
-        print("-- Generation %i --" % g)
-
-        # Select the next generation individuals
-        offspring = toolbox.select(pop, len(pop))
-        # Clone the selected individuals
-        offspring = list(map(toolbox.clone, offspring))
-    
-        # Apply crossover and mutation on the offspring
-        for child1, child2 in zip(offspring[::2], offspring[1::2]):
-
-            # cross two individuals with probability CXPB
-            if random.random() < CXPB:
-                toolbox.mate(child1, child2)
-
-                # fitness values of the children
-                # must be recalculated later
-                del child1.fitness.values
-                del child2.fitness.values
-
-        for mutant in offspring:
-
-            # mutate an individual with probability MUTPB
-            if random.random() < MUTPB:
-                toolbox.mutate(mutant)
-                del mutant.fitness.values
-    
-        # Evaluate the individuals with an invalid fitness
-        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
-        fitnesses = map(toolbox.evaluate, invalid_ind)
-        for ind, fit in zip(invalid_ind, fitnesses):
+        # create an initial population of 300 individuals (where
+        # each individual is a list of integers)
+        pop = toolbox.population(n=POPULATION)
+        
+        # CXPB  is the probability with which two individuals
+        #       are crossed
+        #
+        # MUTPB is the probability for mutating an individual
+        CXPB, MUTPB = 0.9, 0.3
+        
+        print("Start of evolution")
+        
+        # Evaluate the entire population
+        fitnesses = list(map(toolbox.evaluate, pop))
+        for ind, fit in zip(pop, fitnesses):
             ind.fitness.values = fit
         
-        print("  Evaluated %i individuals" % len(invalid_ind))
-        
-        # The population is entirely replaced by the offspring
-        pop[:] = offspring
-        
-        # Gather all the fitnesses in one list and print the stats
+        print("  Evaluated %i individuals" % len(pop))
+
+        # Extracting all the fitnesses of 
         fits = [ind.fitness.values[0] for ind in pop]
+
+        # Variable keeping track of the number of generations
+        g = 0
         
-        length = len(pop)
-        mean = sum(fits) / length
-        sum2 = sum(x*x for x in fits)
-        std = abs(sum2 / length - mean**2)**0.5
+        bestGen = []
         
-        print("  Min %s" % min(fits))
-        print("  Max %s" % max(fits))
-        print("  Avg %s" % mean)
-        print("  Std %s" % std)
+        # Begin the evolution
+        while g < GENERATIONS:
+
+            # A new generation
+            g = g + 1
+            print("-- Generation %i, Run %i--" % (g, rnd))
+
+            # Select the next generation individuals
+            offspring = toolbox.select(pop, len(pop))
+            # Clone the selected individuals
+            offspring = list(map(toolbox.clone, offspring))
+        
+            # Apply crossover and mutation on the offspring
+            for child1, child2 in zip(offspring[::2], offspring[1::2]):
+
+                # cross two individuals with probability CXPB
+                if random.random() < CXPB:
+                    toolbox.mate(child1, child2)
+
+                    # fitness values of the children
+                    # must be recalculated later
+                    del child1.fitness.values
+                    del child2.fitness.values
+
+            for mutant in offspring:
+
+                # mutate an individual with probability MUTPB
+                if random.random() < MUTPB:
+                    toolbox.mutate(mutant)
+                    del mutant.fitness.values
+        
+            # Evaluate the individuals with an invalid fitness
+            invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+            fitnesses = map(toolbox.evaluate, invalid_ind)
+            for ind, fit in zip(invalid_ind, fitnesses):
+                ind.fitness.values = fit
+            
+            print("  Evaluated %i individuals" % len(invalid_ind))
+            
+            # The population is entirely replaced by the offspring
+            pop[:] = offspring
+            
+            # Gather all the fitnesses in one list and print the stats
+            fits = [ind.fitness.values[0] for ind in pop]
+            
+            length = len(pop)
+            mean = sum(fits) / length
+            sum2 = sum(x*x for x in fits)
+            std = abs(sum2 / length - mean**2)**0.5
+            
+            print("  Min %s" % min(fits))
+            print("  Max %s" % max(fits))
+            print("  Avg %s" % mean)
+            print("  Std %s" % std)
+            
+            best_ind = tools.selBest(pop, 1)[0]
+            print("Gen %d: best individual is %s, %s" % (g, best_ind, best_ind.fitness.values))
+            bestGen.append(best_ind.fitness.values)
+
+        
+        print("-- End of (successful) evolution --")
         
         best_ind = tools.selBest(pop, 1)[0]
-        print("Gen %d: best individual is %s, %s" % (g, best_ind, best_ind.fitness.values))
-        bestGen.append(best_ind.fitness.values)
+        print("Best individual is %s, %s" % (best_ind, best_ind.fitness.values))
+        overall_mean += mean
+        overall_std += std
 
-    
-    print("-- End of (successful) evolution --")
-    
-    best_ind = tools.selBest(pop, 1)[0]
-    print("Best individual is %s, %s" % (best_ind, best_ind.fitness.values))
+    overall_mean = overall_mean/MAX_RUNS
+    overall_std = overall_std/MAX_RUNS
 
+    print("MEAN = %i STD = %i" % (overall_mean, overall_std))
+    
     plt.title('Optimized tour')
 
     plt.scatter(positions["x"], positions["y"])
